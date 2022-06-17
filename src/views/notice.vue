8fc0c5b31f6987d947e1f6287f608d12857e79ad
olina-ly <17610615030@163.com>
2022-06-17 18:49:58 +0800

    fix

--- src/views/notice.vue
+++ src/views/notice.vue
@@ -12,7 +12,7 @@
         <div v-else-if="list.length>0" class="list">
           <template v-for="item in list">
             <!-- reply -->
-            <div v-if="item.type=='comment' && item.data.count==0" class="comment-item" @click="redirectPage('/detail/'+item.comment.postId+'?comment='+item.comment.target_hash,false)">
+            <div v-if="item.type=='comment' && item.data.count==0" class="content-item" @click="redirectPage('/detail/'+item.comment.postId+'?comment='+item.comment.target_hash,false)">
               <div class="user">
                 <el-popover placement="bottom-start"  trigger="hover" @show="item.showUser=true" @hide="item.showUser=false">
                   <template #reference>
@@ -37,6 +37,38 @@
                 </div>
               </div>
               <div class="reply-user">Replying to you</div> 
+              <pre v-if="item.type != 'encrypt' || item.isAccess" class="text"><div v-html="item.text"></div></pre>
+              <div v-else class="default-content">
+                This is a Tonken-gated content.
+              </div>
+            </div>
+
+            <!-- @ -->
+            <div v-else-if="item.type=='at' && item.data.count==0" class="content-item" @click="redirectPage('/detail/'+item.comment.postId+'?comment='+item.comment.target_hash,false)">
+              <div class="user">
+                <el-popover placement="bottom-start"  trigger="hover" @show="item.showUser=true" @hide="item.showUser=false">
+                  <template #reference>
+                    <img v-if="item.user.avatar" class="avatar" :src="item.user.avatar" @click.stop="redirectPage('/user-profile/'+item.accountId,false)"/>
+                    <img v-else  class="avatar" src="@/assets/images/common/user-default.png" @click.stop="redirectPage('/user-profile/'+item.accountId,false)"/>
+                  </template>
+                  <template v-if="item.showUser">
+                    <UserPopup :account="item.accountId"/>
+                  </template>
+                </el-popover>
+
+                <div class="user-info">
+                  <div class="name txt-wrap" @click.stop="redirectPage('/user-profile/'+item.accountId,false)">
+                    {{item.user.name || item.accountId}}
+                  </div> <br/>
+                  <el-popover placement="bottom-start"  trigger="hover">
+                    <template #reference>
+                      <div class="createtime">{{item.time.showTime}}</div>
+                    </template>
+                    <div class="pop-box pop-tip">{{item.time.hoverTime}}</div>
+                  </el-popover>
+                </div>
+              </div>
+              <div class="reply-user"><span>@you</span></div> 
               <pre v-if="item.type != 'encrypt' || item.isAccess" class="text"><div v-html="item.text"></div></pre>
               <div v-else class="default-content">
                 This is a Tonken-gated content.
@@ -320,7 +352,7 @@ export default {
       >div:last-child{
         border:0;
       }
-      .comment-item{
+      .content-item{
         padding:20px 0;
         border-bottom:1px solid rgba(255,255,255,0.1);
         cursor: pointer;
@@ -488,7 +520,7 @@ export default {
         }
       }
       .follow-item{
-        background:url('@/assets/images/notice/icon-users.png') no-repeat left 30px;
+        background:url('@/assets/images/notice/icon-users.png') no-repeat left 20px;
         background-size:40px 40px;
       }
 
